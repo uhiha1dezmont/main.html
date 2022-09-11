f7205d088792ef45b3e744cc0d856564f6371781
uhiha1dezmont <93062819+uhiha1dezmont@users.noreply.github.com>
2022-09-11 13:36:34 +0300

    Add files via upload

---
+++ websi/modyls.app/test.py
@@ -0,0 +1,5 @@
+#target="_blank>
+#<a href="https://vk.com/weronnip" title="Разработчик сайта" target="_blank"><img id="center_img" src="image/vklogo.png" alt="" width="80" height="80"></a>
+#background-image: url(header/header6.png)
+#<a href="#" title="Телеграмм бот"><img id="center_img" src="image/telelogo.png" alt="" width="80" height="80"></a>
+#<a href="#" title="Вк бот"><img id="center_img" src="image/vklogo.png" alt="" width="80" height="80"></a>
